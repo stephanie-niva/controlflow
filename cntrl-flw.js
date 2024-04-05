5483aff2043a4217ae2a78631b9fb211aa268bb6
stephanie-niva <stephanienivaa@example.com>
2024-04-05 12:07:03 +0300

    add files

---
+++ cntrl-flw.js
@@ -0,0 +1,16 @@
+//Q1
+function check(){
+    let nums= [1,2,3,4,5,6]
+    for(var i=0;i<nums.length;i++){
+        if(i<4){
+        let a=nums[i];
+        console.log(a*a)
+        }
+        else{
+         let b= nums[i]
+         console.log(b+10)
+        }
+    }
+}
+console.log(check())
+//Q2
