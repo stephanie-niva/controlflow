97fd55f89651228c07316a127b7acf2c51e284c6
stephanie-niva <stephanienivaa@example.com>
2024-04-05 17:15:36 +0300

    add changes

--- cntrl-flw.js
+++ cntrl-flw.js
@@ -14,3 +14,53 @@ function check(){
 }
 console.log(check())
 //Q2
+let arrNum = [1,2,3,4,5,6,7,8,9];
+
+function operations(nums){
+    let newArr = arrNum.length
+    let i = 0;
+    while(i<newArr){
+        if(nums[i]===3){
+            break;
+
+        }
+        else{
+            console.log(nums[i]);
+        }
+        i++;
+    }
+}
+operations(arrNum);
+
+//Q3
+let fruits=['apple','plum','banana','strawberries','kiwi'];
+
+function arrayString(x){
+    for (let i=0; i<x.length; i++){
+        if(i===2){
+            continue;
+        }else{
+            console.log(x[i])
+        }
+    }
+}
+arrayString(fruits)
+//Q4
+let colours = ["blue","black","green","white"]
+function arrayString(arr){
+    for(let i=0; i<arr.length; i++){
+        console.log(arr[i]);
+    }
+}
+arrayString(colours)
+//Q5
+function reverse(item){
+    let word = true;
+    while (word){
+        console.log(item.split("").reverse().join(""));
+         
+        break;
+
+    }
+}
+reverse("school")
